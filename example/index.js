700463e8fa2c7b25ef3a638ecb00a6920d52423c
metal3d <metal3d@gmail.com>
2013-04-26 01:26:27 +0200

    Example follow the new handler creation system

--- example/index.js
+++ example/index.js
@@ -5,46 +5,30 @@
 
 // outside example, use require('knotter')
 var knotter = require('../index');
-
-
-/**
- * Handler that can respond to /page1
- *
- */
-var h1 = new knotter.Handler({
-    route : '^/page1',
-
-    get: function(){
-       console.log("on page1, ok");
-       this.response.end("done");
-    }
-    
-});
+var util    = require('util')
 
 /**
- * Handler that respond to /page/anything.../number
- * and can handle sessions
- *
+ * A test handler
  */
-var h2 = new knotter.Handler({
-    route : '^/page/(.+?)/(\\d+)',
-    useSessions: true,
+function TestHandler(){
+    knotter.Handler.call(this);
+}
 
-    get: function (){
-        var ctx = {params: this.params.args};
-        if (!this.sessions.get('count')) {
-            this.sessions.set('count', 0);
-        }
-        this.sessions.set('count', this.sessions.get('count')+1);
-        ctx.counter = this.sessions.get('count');
-        console.log("on page with context, ok");
-        this.response.end(JSON.stringify(ctx));
-    }
-});
+
+util.inherits(TestHandler, knotter.Handler);
+
+
+TestHandler.prototype.route = "^/page/test";
+
+TestHandler.prototype.get = function (){
+    this.write("hello on page 1");
+    this.end();
+};
+
 
 
 var server = new knotter.Server({
-    handlers : [h1, h2],
+    handlers : [TestHandler],
     statics : {"styles": "example/css"}
 });
 
